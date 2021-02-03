1e9e4793c3c7900c7facaf7942ed1b0b050189f6
Adetayo <akinsanyaadetayo7@gmail.com>
2021-02-03 22:46:26 +0100

    FEAT:Add details component
    Signed-off-by: Adetayo <akinsanyaadetayo7@gmail.com>

---
+++ src/styles.js
@@ -0,0 +1,55 @@
+// import { makeStyles } from '@material-ui/core/styles';
+
+// export default makeStyles((theme) => ({
+//   media: {
+//     height: 0,
+//     paddingTop: '56.25%', // 16:9
+//   },
+//   expand: {
+//     transform: 'rotate(0deg)',
+//     marginLeft: 'auto',
+//     transition: theme.transitions.create('transform', {
+//       duration: theme.transitions.duration.shortest,
+//     }),
+//   },
+//   expandOpen: {
+//     transform: 'rotate(180deg)',
+//   },
+//   cardContent: {
+//     paddingTop: 0,
+//   },
+//   divider: {
+//     margin: '20px 0',
+//   },
+// }));
+
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  desktop: {
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  mobile: {
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
+  },
+  main: {
+    [theme.breakpoints.up('sm')]: {
+      paddingBottom: '5%',
+    },
+  },
+  last: {
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: theme.spacing(3),
+      paddingBottom: '200px',
+    },
+  },
+  grid: {
+    '& > *': {
+      margin: theme.spacing(2),
+    },
+  },
+}));
