d61d92492d1387e8db214162dba145c03b1854d1
Adetayo <akinsanyaadetayo7@gmail.com>
2021-02-03 22:55:50 +0100

    Added title props for each card
    Signed-off-by: Adetayo <akinsanyaadetayo7@gmail.com>

---
+++ src/Components/Details/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Divider,
+} from '@material-ui/core';
+
+const Main = () => {
+  return <div></div>;
+};
+
+export default Main;
