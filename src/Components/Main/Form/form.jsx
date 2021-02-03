302ef4667074b5157fb3a4e8454a5b21e45da6e6
Adetayo <akinsanyaadetayo7@gmail.com>
2021-02-03 23:09:06 +0100

    Added the Main Component and fixed styling]

---
+++ src/Components/Main/Form/form.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  TextField,
+  Typography,
+  Grid,
+  Button,
+  FormControl,
+  TextLabel,
+  Select,
+  MenuItem,
+} from '@material-ui/core';
+
+const form = () => {
+  return <div></div>;
+};
+
+export default form;
