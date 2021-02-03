5cc2c635f6404d490fa67fb0a30bcd83672dbe7a
Adetayo <akinsanyaadetayo7@gmail.com>
2021-02-03 23:26:18 +0100

    Implement the form structure

--- src/Components/Main/Form/form.jsx
+++ src/Components/Main/Form/form.jsx
@@ -7,11 +7,28 @@ import {
   FormControl,
   TextLabel,
   Select,
+  InputLabel,
   MenuItem,
 } from '@material-ui/core';
-
+import useStyles from './styles';
 const form = () => {
-  return <div></div>;
+  const classes = useStyles();
+  return (
+    <Grid container spcaing={2}>
+      <Grid item xs={12}>
+        <Typography align='center' variant='subtitle2' gutterBottom>
+          ...
+        </Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <FormControl fullWidth>
+          <InputLabel>Type</InputLabel>
+          
+        
+        </FormControl>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default form;
